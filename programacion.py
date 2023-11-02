eb861e939909a9e05cc5ed8befe9d10a7d718629
Livay <milton.lopez.c20@gmail.com>
2023-11-01 23:26:50 -0300

    creacion proyecto prueba

---
+++ programacion.py
@@ -0,0 +1,43 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: No se puede dividir por cero."
+
+while True:
+    print("Opciones:")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("5. Salir")
+    
+    opcion = input("Elige una opción (1/2/3/4/5): ")
+    
+    if opcion == "5":
+        print("¡Hasta luego!")
+        break
+
+    if opcion in ("1", "2", "3", "4"):
+        num1 = float(input("Ingresa el primer número: "))
+        num2 = float(input("Ingresa el segundo número: "))
+
+        if opcion == "1":
+            print("Resultado: ", suma(num1, num2))
+        elif opcion == "2":
+            print("Resultado: ", resta(num1, num2))
+        elif opcion == "3":
+            print("Resultado: ", multiplicacion(num1, num2))
+        elif opcion == "4":
+            print("Resultado: ", division(num1, num2))
+    else:
+        print("Opción no válida. Por favor, elige una opción válida (1/2/3/4/5).")
